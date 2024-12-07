1c967718b66c2307e1b5441ed3ae5dadf4a006b8
hellopatrick; Patrick Rogers <hellopatrick@users.noreply.github.com>
2024-12-07 07:33:21 -0800

    gen day 8

---
+++ exe/2024-08/main.cpp
@@ -0,0 +1,22 @@
+#include "lib.h"
+
+#include <chrono>
+#include <print>
+
+int parse_stdin() { return 0; }
+
+int main() {
+    auto data = parse_stdin();
+
+    auto start = std::chrono::high_resolution_clock::now();
+
+    auto part1 = 0;
+    auto part2 = 0;
+
+    auto end = std::chrono::high_resolution_clock::now();
+    auto dur = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+
+    std::print("part 1: {}\n", part1);
+    std::print("part 2: {}\n", part2);
+    std::print("dur:    {}us\n", dur.count());
+}
